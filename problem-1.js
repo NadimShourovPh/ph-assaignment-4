0b2891cb34a15d0c3abbbd96c6818dc0a36da058
01919108033 <nadimshourov8@gmail.com>
2024-02-07 22:30:42 +0600

    problem-1

---
+++ problem-1.js
@@ -0,0 +1,12 @@
+function calculateMoney(ticket ) {
+    let gurdBill = 500;
+   let perPersonLunch = 50;
+   let eightPersonLunchBill = perPersonLunch*8
+   let perDayTotalCost = gurdBill+eightPersonLunchBill;
+   let ticketPrice = 120;
+   let perDayIncome = (ticketPrice*ticket)-perDayTotalCost
+   return perDayIncome
+}
+
+let ticketSell = calculateMoney( 120)
+console.log(ticketSell)
